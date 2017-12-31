e522574ece1457c8f39a92a46c9f1e411e792020
Kahdeg-15520487 <15520487@gm.uit.edu.vn>
2017-12-31 12:35:00 +0700

    Added logger

---
+++ 2.5D/Logger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Utilities;
+
+namespace Utilities{
+	partial class CONTENT_MANAGER{
+		public static void Log(string log) {
+
+		}
+	}
+}
